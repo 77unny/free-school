35b27ccc872c31157115004849f5e300ffe66c0e
baekCode <sosaer88@gmail.com>
2020-01-01 22:30:14 +0900

    add, js-todos.js

---
+++ js-array2.js
@@ -0,0 +1,13 @@
+// 배열 메소드
+// pop push unshift shift splice
+// concat join split slice indexOf
+
+
+const text = '자바스크립트, 프론트엔드, HTML, CSS';
+const splitArray = text.split(', ');
+console.log(splitArray);
+
+const text2 = ['자바', '자바스크립트', '스프링']
+const joinArray = text2.join(', ');
+console.log(joinArray);
+
